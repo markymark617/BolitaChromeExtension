31e3053cd6d97c4168fb5c53e8ae171f4911d708
markymark617 <mark.flanagan.updates@gmail.com>
2020-09-12 22:59:30 -0400

    beginning web3 integration

--- popup.js
+++ popup.js
@@ -61,7 +61,13 @@ document.querySelectorAll(".btnThirdBet").forEach(item => {
     })
 });
 
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+} else {
+    web3 = new Web3( new Web3.providers.HttpProviders("http://localhost:8545"));
+}
 
+web3.eth.defaultAccount = web3.eth.accounts[0];
 
 
 /*
