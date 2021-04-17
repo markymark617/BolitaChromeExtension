2de7e9eb1b6118c624bf82f6a84fa3d494e275bd
markymark617 <mark.flanagan.updates@gmail.com>
2021-04-17 17:18:26 -0400

    cleaning up JS code, and documenting better

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
 
+/**
+ * For: FIRST ROW OF BUTTONS
+ * make each button clickable, turn lighter when clicked, 
+ * and set the value of the corresponding input field
+ */
 document.querySelectorAll(".btnFirstBet").forEach(item => {
     item.addEventListener('click', event => {
         //handle click
@@ -19,8 +24,11 @@ document.querySelectorAll(".btnFirstBet").forEach(item => {
     })
 });
 
-
-
+/**
+ * For: SECOND ROW OF BUTTONS
+ * make each button clickable, turn lighter when clicked, 
+ * and set the value of the corresponding input field
+ */
 document.querySelectorAll(".btnSecondBet").forEach(item => {
     item.addEventListener('click', event => {
         //handle click
@@ -41,7 +49,11 @@ document.querySelectorAll(".btnSecondBet").forEach(item => {
     })
 });
 
-
+/**
+ * For: THIRD ROW OF BUTTONS
+ * make each button clickable, turn lighter when clicked, 
+ * and set the value of the corresponding input field
+ */
 document.querySelectorAll(".btnThirdBet").forEach(item => {
     item.addEventListener('click', event => {
         //handle click
@@ -62,7 +74,7 @@ document.querySelectorAll(".btnThirdBet").forEach(item => {
     })
 });
 
-
+//Button IDs on second page
 var latestNums = document.getElementById("finalNumbersID");
 var btnLatestNums = document.getElementById("GetLatestNumbers");
 
@@ -70,11 +82,8 @@ var btnLatestNums = document.getElementById("GetLatestNumbers");
 window.onload = function(){ 
 
     btnLatestNums.onclick = function() {
-
         latestNums.innerHTML = "Hello World";
-
     };
-
 };
 
 
@@ -87,37 +96,31 @@ window.onload = function(){
     btnGetCNNNumbers.onclick = function() {
         
         latestNums.innerHTML = "Hello CNN";
-        
+            
+        //const json = '{"result":true, "count":42}';
+        var url = "https://api.covidtracking.com/v1/us/current.json";
 
+        var obj;
+        var positiveCasesNum;
 
-    //const json = '{"result":true, "count":42}';
-    var url = "https://api.covidtracking.com/v1/us/current.json";
+        //API call
+        $.getJSON('https://api.covidtracking.com/v1/us/current.json', function(data) {
+            // JSON result in `data` variable
+            console.log(data);
+            console.log("TADA!");
+            console.log(data[0].positive);
+            positiveCasesNum = data[0].positive;
+            console.log("---");
+            obj = data;
 
-    var obj;
-    var positiveCasesNum;
-
-    $.getJSON('https://api.covidtracking.com/v1/us/current.json', function(data) {
-        // JSON result in `data` variable
-        console.log(data);
-        console.log("TADA");
-        console.log(data[0].positive);
-        positiveCasesNum = data[0].positive;
-        console.log("---");
-        obj = data;
-
-
-        latestCaseNum.innerHTML = data[0].positive;
-        console.log("<p> tag updated!");
-        
+            latestCaseNum.innerHTML = data[0].positive;
+            console.log("<p> tag updated!");
         });
 
     };
 };
 
-
 var sendNotificationbtn = document.getElementById("btnSendNotification");
-
-
 var options = {
     title:'myNotification',
     message: 'TIMING NOTIFICATION',
@@ -127,63 +130,65 @@ var options = {
 
 //added for error on Liveserver 
 window.onload = function(){ 
-    
     chrome.notifications.create(options);
 };
 
-    function greeting() {
-        console.log('Hello World!');
-      }
-      
-      setTimeout(greeting, 1000);
+
+//testing function
+function greeting() {
+    console.log('Hello World!');
+}
     
+setTimeout(greeting, 1000);
+
+
+//clock function 
+function updateLatestWinningNumbers() {
+    (function loop() {
     
-    function updateLatestWinningNumbers() {
-        (function loop() {
-       
-            var now = new Date();
-            if (now.getHours() === 20 && now.getMinutes() === 08) {
-                latestNums.innerHTML = "Hello World TIMING";
-                console.log('TIMING RAN!');
+        var now = new Date();
+        if (now.getHours() === 20 && now.getMinutes() === 08) {
+            latestNums.innerHTML = "Hello World TIMING";
+            console.log('TIMING RAN!');
+
+            var notifOptions = {
+                type: 'basic',
+                iconUrl: 'images/bolita_logo.png',
+                title: 'Time reached',
+                message: "TIMING boom baby"
+            };
+
+            chrome.notifications.create('timeNotif', notifOptions);
+        }
+
+        now = new Date();                  // allow for time passing
+        var delay = 60000 - (now % 60000); // exact ms to next minute interval
+        setTimeout(updateLatestWinningNumbers, delay);
+
+        }
+    )();
+}
+
+//run clock function
+updateLatestWinningNumbers();
+
+function updateClock() {
+    var now = new Date(), // current date
+        time = now.getHours() + ':' + now.getMinutes() +"::" + now.getSeconds();
+
+        // a cleaner way than string concatenation
+        date = [now.getDate(), 
+                now.getFullYear()].join(' ');
+
+    // set the content of the element with the ID time to the formatted string
+    document.getElementById('time').innerHTML =  now;
+
+    // call this function again in 1000ms
+    setInterval(updateClock, 1000);
+}
+updateClock(); // initial call
     
-                var notifOptions = {
-                    type: 'basic',
-                    iconUrl: 'images/bolita_logo.png',
-                    title: 'Time reached',
-                    message: "TIMING boom baby"
-                };
-    
-                chrome.notifications.create('timeNotif', notifOptions);
-            }
-    
-            now = new Date();                  // allow for time passing
-            var delay = 60000 - (now % 60000); // exact ms to next minute interval
-            setTimeout(updateLatestWinningNumbers, delay);
-    
-            }
-        )();
-    }
-    
-    updateLatestWinningNumbers();
-    
-    function updateClock() {
-        var now = new Date(), // current date
-            time = now.getHours() + ':' + now.getMinutes() +"::" + now.getSeconds();
-    
-            // a cleaner way than string concatenation
-            date = [now.getDate(), 
-                    now.getFullYear()].join(' ');
-    
-        // set the content of the element with the ID time to the formatted string
-        document.getElementById('time').innerHTML =  now;
-    
-        // call this function again in 1000ms
-        setInterval(updateClock, 1000);
-    }
-    updateClock(); // initial call
-    
-    
-    
+
 
     /*
         web3 changes
@@ -191,7 +196,6 @@ window.onload = function(){
 
 //added since error on web3 may be from race condition
 window.addEventListener('load', function() {
- //   var Web3 = require('web3');
 
     if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
@@ -202,6 +206,7 @@ window.addEventListener('load', function() {
     web3.eth.defaultAccount = web3.eth.accounts[0];
 
     //var BolitaContract = web3.eth.contract([ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "newAdminAddress", "type": "address" } ], "name": "AddedAdmin", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "newOwnerAddress", "type": "address" } ], "name": "OwnerSet", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Paused", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "prevAdminAddress", "type": "address" } ], "name": "RemovedAdmin", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Unpaused", "type": "event" }, { "inputs": [], "name": "adminAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ownerAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "pause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "paused", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_newAdminAddress", "type": "address" } ], "name": "setAdmin", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "unpause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_newOwnerAddress", "type": "address" } ], "name": "updateOwner", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ])
+    // address changes with each deploy on Remix, and would be a fixed addr if was deployed to testnet or mainnet
     var BolitaContract = new web3.eth.Contract([
         {
             "inputs": [],
@@ -711,153 +716,11 @@ window.addEventListener('load', function() {
     ],
     '0x1a9dEBC644B9854d44B56bAE1c9cEDDeeba494c6');
 
-
-
-    //changes with each deploy on Remix, obv would be fixed if was deployed to testnet or mainnet
-    //0.x.x syntax - now outdated
-    //var Bolita = BolitaContract.at('0x1a9dEBC644B9854d44B56bAE1c9cEDDeeba494c6');
-
     console.log(BolitaContract);
 
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*if(latestNums) {
-    
-    latestNums.addEventListener("click", function() {
-
-        document.getElementById("finalNumbersID").innerHTML = "Hello World";
-    
-        }
-    );
-}
-
-
-
-    function surprise(cb) {
-        (function loop() {
-            var now = new Date();
-            if (now.getHours() === 19 && now.getMinutes() === 27) {
-                latestNums.innerHTML = "Hello World TIMING";
-            }
-            now = new Date();                  // allow for time passing
-            var delay = 60000 - (now % 60000); // exact ms to next minute interval
-            setTimeout(loop, delay);
-        })();
-    }
-
-
-
-
-    <script>
-
-
-
-       /* var headerToBeReplaced = document.getElementById('finalNumbersID');
-        headerToBeReplaced.innerHTML = headerToBeReplaced.innerHTML.replace('some header val','changedtext')
-
-
-       const resultsList=document.getElementById('results')
-        new URLSearchParams(window.location.search).forEach((value,
-        name) => {
-            resultsList.append('${name} ${value}')
-            resultsList.append(document.createElement('br'))
-        })
-        
-        
-
-
-
-    script
-        jQuery('finalNumbersID').load('122333');
-    script
-
-        
-        
-        
-        
-        */
-
-
-/*
-window.setInterval(function(){
-    var date = new Date(); 
-    if(date.getHours() === 16 && date.getMinutes() === 45){ 
-        // Do stuff
-    }
-}, 60000);
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//WEB3 CODE
-
-/*
-
-
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-} else {
-    web3 = new Web3( new Web3.providers.HttpProvider("http://localhost:8545"));
-}
-
-web3.eth.defaultAccount = web3.eth.accounts[0];
-
-
-
-*/
-
-
-
 
 
 
